fadb4b7ec693fd4a0b1d88e522ae5f36bd0fab52
Dave Cheong <dcheong@deloitte.com.au>
2016-08-26 11:01:46 +1000

    RAM-117 New db model

--- backend/typescript/models/legislativeProgram.model.ts
+++ backend/typescript/models/legislativeProgram.model.ts
@@ -1,67 +1,84 @@
 import * as mongoose from 'mongoose';
-import {ICodeDecode, CodeDecodeSchema} from './base';
+import {ICodeDecode, CodeDecodeSchema, Model} from './base';
 
 // enums, utilities, helpers ..........................................................................................
 
+// exports ............................................................................................................
+
+export interface ILegislativeProgram extends ICodeDecode, ILegislativeProgramInstanceContract {
+}
+
+export interface ILegislativeProgramModel extends mongoose.Model<ILegislativeProgram>, ILegislativeProgramStaticContract {
+}
+
+export let LegislativeProgramModel: ILegislativeProgramModel;
+
 // schema .............................................................................................................
 
-const LegislativeProgramSchema = CodeDecodeSchema({
-});
+const LegislativeProgramSchema = CodeDecodeSchema({});
 
-// interfaces .........................................................................................................
+// instance ...........................................................................................................
 
-export interface ILegislativeProgram extends ICodeDecode {
+export interface ILegislativeProgramInstanceContract {
 }
 
-export interface ILegislativeProgramModel extends mongoose.Model<ILegislativeProgram> {
-    findByCodeIgnoringDateRange: (code:String) => Promise<ILegislativeProgram>;
-    findByCodeInDateRange: (code:String, date:Date) => Promise<ILegislativeProgram>;
-    listIgnoringDateRange: () => Promise<ILegislativeProgram[]>;
-    listInDateRange: (date:Date) => Promise<ILegislativeProgram[]>;
+class LegislativeProgramInstanceContractImpl implements ILegislativeProgramInstanceContract {
 }
 
-// instance methods ...................................................................................................
+// static .............................................................................................................
 
-// static methods .....................................................................................................
+interface ILegislativeProgramStaticContract {
+    findByCodeIgnoringDateRange: (code: String) => mongoose.Promise<ILegislativeProgram>;
+    findByCodeInDateRange: (code: String, date: Date) => mongoose.Promise<ILegislativeProgram>;
+    listIgnoringDateRange: () => mongoose.Promise<ILegislativeProgram[]>;
+    listInDateRange: (date: Date) => mongoose.Promise<ILegislativeProgram[]>;
+}
 
-LegislativeProgramSchema.static('findByCodeIgnoringDateRange', (code:String) => {
-    return this.LegislativeProgramModel
-        .findOne({
-            code: code
-        })
-        .exec();
-});
+class LegislativeProgramStaticContractImpl implements ILegislativeProgramStaticContract {
 
-LegislativeProgramSchema.static('findByCodeInDateRange', (code:String, date:Date) => {
-    return this.LegislativeProgramModel
-        .findOne({
-            code: code,
-            startDate: {$lte: date},
-            $or: [{endDate: null}, {endDate: {$gte: date}}]
-        })
-        .exec();
-});
+    public findByCodeIgnoringDateRange(code: string): mongoose.Promise<ILegislativeProgram> {
+        return LegislativeProgramModel
+            .findOne({
+                code: code
+            })
+            .exec();
+    }
 
-LegislativeProgramSchema.static('listIgnoringDateRange', () => {
-    return this.LegislativeProgramModel
-        .find({
-        })
-        .sort({shortDecodeText: 1})
-        .exec();
-});
+    public findByCodeInDateRange(code: string, date: Date): mongoose.Promise<ILegislativeProgram> {
+        return LegislativeProgramModel
+            .findOne({
+                code: code,
+                startDate: {$lte: date},
+                $or: [{endDate: null}, {endDate: {$gte: date}}]
+            })
+            .exec();
+    }
 
-LegislativeProgramSchema.static('listInDateRange', (date:Date) => {
-    return this.LegislativeProgramModel
-        .find({
-            startDate: {$lte: date},
-            $or: [{endDate: null}, {endDate: {$gte: date}}]
-        })
-        .sort({shortDecodeText: 1})
-        .exec();
-});
+    public listIgnoringDateRange(): mongoose.Promise<ILegislativeProgram[]> {
+        return LegislativeProgramModel
+            .find({})
+            .sort({shortDecodeText: 1})
+            .exec();
+    }
+
+    public listInDateRange(date: Date): mongoose.Promise<ILegislativeProgram[]> {
+        return LegislativeProgramModel
+            .find({
+                startDate: {$lte: date},
+                $or: [{endDate: null}, {endDate: {$gte: date}}]
+            })
+            .sort({shortDecodeText: 1})
+            .exec();
+    }
+
+}
 
 // concrete model .....................................................................................................
 
-export const LegislativeProgramModel = mongoose.model(
-    'LegislativeProgram',
-    LegislativeProgramSchema) as ILegislativeProgramModel;
+LegislativeProgramModel =
+    Model(
+        'LegislativeProgram',
+        LegislativeProgramSchema,
+        LegislativeProgramInstanceContractImpl,
+        LegislativeProgramStaticContractImpl
+    ) as ILegislativeProgramModel;
