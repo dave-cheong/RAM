bb6e30fea0110d2c3055dae1c68494fd15d0ac15
Dave Cheong <dcheong@deloitte.com.au>
2016-09-07 09:25:24 +1000

    RAM-71 Removed unused import

--- backend/typescript/models/name.model.ts
+++ backend/typescript/models/name.model.ts
@@ -1,9 +1,6 @@
 import * as mongoose from 'mongoose';
 import {RAMSchema, IRAMObject, RAMObject, Model} from './base';
-import {
-    HrefValue,
-    Name as DTO
-} from '../../../commons/api';
+import {Name as DTO} from '../../../commons/api';
 import {Permissions} from '../../../commons/dtos/permission.dto';
 import {PermissionTemplates} from '../../../commons/permissions/allPermission.templates';
 import {PermissionEnforcers} from '../permissions/allPermission.enforcers';
@@ -62,7 +59,6 @@ export interface IName extends IRAMObject {
     familyName?: string;
     unstructuredName?: string;
     _displayName?: string;
-    toHrefValue(includeValue: boolean): Promise<HrefValue<DTO>>;
     toDTO(): Promise<DTO>;
 }
 
@@ -75,13 +71,6 @@ class Name extends RAMObject implements IName {
 
     public getPermissions(): Promise<Permissions> {
         return this.enforcePermissions(PermissionTemplates.iname, PermissionEnforcers.iname);
-    }
-
-    public async toHrefValue(includeValue: boolean): Promise<HrefValue<DTO>> {
-        return new HrefValue(
-            null, // TODO do these have endpoints?
-            includeValue ? await this.toDTO() : undefined
-        );
     }
 
     public async toDTO(): Promise<DTO> {
