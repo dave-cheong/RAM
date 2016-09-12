a6ec15ff8943216443772d0edeb349a4a0865dc5
Dave Cheong <dcheong@deloitte.com.au>
2016-09-12 16:53:34 +1000

    RAM-118 DOB for pending

--- backend/typescript/permissions/relationshipCanViewDobPermission.enforcer.ts
+++ backend/typescript/permissions/relationshipCanViewDobPermission.enforcer.ts
@@ -3,7 +3,8 @@ import {IPermission, Permission} from '../../../commons/dtos/permission.dto';
 import {
     RelationshipCanViewDobPermission
 } from '../../../commons/permissions/relationshipPermission.templates';
-import {IRelationship} from '../models/relationship.model';
+import {IRelationship, RelationshipStatus} from '../models/relationship.model';
+import {Translator} from '../ram/translator';
 
 export class RelationshipCanViewDobPermissionEnforcer extends PermissionEnforcer<IRelationship> {
 
@@ -14,8 +15,19 @@ export class RelationshipCanViewDobPermissionEnforcer extends PermissionEnforcer
     public async evaluate(relationship: IRelationship): Promise<IPermission> {
 
         let permission = new Permission(this.template.code, this.template.description, this.template.value, this.template.linkType);
+        let relationshipStatus = relationship.statusEnum();
 
-        permission.value = false;
+        // validate pending
+        if (relationshipStatus !== RelationshipStatus.Pending) {
+            permission.messages.push(Translator.get('relationship.canViewDob.invalidStatus'));
+        }
+
+        // set value and link
+        if (permission.messages.length === 0) {
+            permission.value = true;
+        } else {
+            permission.value = false;
+        }
 
         return permission;
 
